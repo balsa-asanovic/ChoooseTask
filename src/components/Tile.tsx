fdb80017fbd567020666e3cf8f15eef54c5176bb
Balsa Asanovic <balsaasanovic95@gmail.com>
2023-09-05 22:13:58 +0200

    added backGround picture style to Tile component

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,23 +1,43 @@
 // src/components/Tile.tsx
 import React from 'react';
-import { Box, Image, Heading, Text } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 interface TileProps {
-  title: string;
-  subtitle: string;
-  image: string;
-  offset: number;
-  rating: number;
+    title: string;
+    subtitle: string;
+    image: string;
+    offset: number;
+    rating: number;
 }
 
-const Tile = ({ title, subtitle, image, offset, rating }: TileProps) => (
-  <Box borderWidth="1px" borderRadius="md" p="4" boxShadow="base">
-    <Image src={image} alt={title} />
-    <Heading>{title}</Heading>
-    <Text>{subtitle}</Text>
-    <Text>Emission Offset: {offset} tons</Text>
-    <Text>Star Rating: {rating}</Text>
-  </Box>
-);
+const Tile = ({ title, subtitle, image, offset, rating }: TileProps) => {
+
+    const tileStyle: React.CSSProperties = {
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover', // Adjust how the image covers the background
+        backgroundPosition: 'center', // Center the background image
+        backgroundRepeat: 'no-repeat' // Prevent image repetition
+    };
+
+    // Define a style object to set the font color to white
+    const textStyle: React.CSSProperties = {
+        color: 'white'
+    };
+
+    return (
+        <Box
+            borderWidth="1px"
+            borderRadius="md"
+            p="4"
+            boxShadow="base"
+            style={tileStyle}
+        >
+            <Heading as="h2" size="md" style={textStyle}>{title}</Heading>
+            <Text style={textStyle}>{subtitle}</Text>
+            <Text style={textStyle}>Emission Offset: {offset} tons</Text>
+            <Text style={textStyle}>Star Rating: {rating}</Text>
+        </Box>
+    )
+};
 
 export default Tile;
